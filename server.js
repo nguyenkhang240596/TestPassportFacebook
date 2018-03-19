6f5d93f0c1debd783238b4d9b12e41cf4a4dd84a
ubuntu <nguyenkhang240596@gmail.com>
2018-03-19 20:03:58 +0700

    init

--- server.js
+++ server.js
@@ -56,14 +56,6 @@ app.get('/', function (req, res) {
 })
 
 app.get('/login',
-  // function(req, res, next) {
-
-  //   console.log(req.isAuthenticated())
-  //   if (req.isAuthenticated()) {
-  //     req.logout();
-  //   } 
-  //   next();
-  // },   
   passport.authenticate('facebook'))
 
 app.get('/logout', function(req, res){
@@ -71,11 +63,6 @@ app.get('/logout', function(req, res){
   res.redirect('/');
 });
 
-// Simple route middleware to ensure user is authenticated.
-//  Use this route middleware on any resource that needs to be protected.  If
-//  the request is authenticated (typically via a persistent login session),
-//  the request will proceed.  Otherwise, the user will be redirected to the
-//  login page.
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   res.send(403)
